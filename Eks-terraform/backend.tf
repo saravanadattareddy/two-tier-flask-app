4be6ef83aa4fb41e52284476c3b6c7e0508b5aa7
saravanadattareddy <127585712+saravanadattareddy@users.noreply.github.com>
2025-08-31 10:06:00 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "mrcloudbooktwotier" # Replace with your actual S3 bucket name
+    bucket = "observability123" # Replace with your actual S3 bucket name
     key    = "EKS-Ajay/terraform.tfstate"
     region = "ap-south-1"
   }
